32a8b288daddb17ee5a9e57df13fc7651cb20b6d
NourhanHamada <nourhan.hamada996@gmail.com>
2023-05-01 15:56:32 +0300

    some changes

--- lib/view/screens/auth/login_screen.dart
+++ lib/view/screens/auth/login_screen.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:tap_cash/constants/colors.dart';
 import 'package:tap_cash/constants/images.dart';
 import 'package:tap_cash/constants/styles.dart';
 import 'package:tap_cash/view/utils/custom_text_form_field.dart';
-
-import '../../../controller/cubit/check_box_cubit.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -42,42 +39,64 @@ class _LoginScreenState extends State<LoginScreen> {
                   width: 180.w,
                 ),
               ),
-             const CustomTextFormField(
-                labelText: 'Email',
-                obscureText: false,
-                textInputType: TextInputType.emailAddress,
-              ),
-             SizedBox(
-               height: 20.h,
-             ),
-             const CustomTextFormField(
-                labelText: 'Password',
-                obscureText: false,
-                textInputType: TextInputType.visiblePassword,
+              Form(
+                child: Column(
+                  children: [
+                    const CustomTextFormField(
+                      labelText: 'Email',
+                      obscureText: false,
+                      textInputType: TextInputType.emailAddress,
+                    ),
+                    SizedBox(
+                      height: 20.h,
+                    ),
+                    const CustomTextFormField(
+                      labelText: 'Password',
+                      obscureText: false,
+                      textInputType: TextInputType.visiblePassword,
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 height: 15.h,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     children: [
-                      Checkbox(value: isChecked,
-                          onChanged: (value){
-                        isChecked = value!;
-                        setState(() {
-                          isChecked != isChecked;
-                        });
-                      },
+                      Checkbox(
+                        value: isChecked,
+                        onChanged: (value) {
+                          isChecked = value!;
+                          setState(() {
+                            isChecked != isChecked;
+                          });
+                        },
                         materialTapTargetSize: MaterialTapTargetSize.padded,
                         visualDensity: VisualDensity.compact,
                       ),
-                       Text('Remember me', style: MyStyles.textStyle14.copyWith(color: MyColors.grey),),
+                      Text(
+                        'Remember me',
+                        style:
+                            MyStyles.textStyle14.copyWith(color: MyColors.grey),
+                      ),
                     ],
                   ),
-
+                  Text(
+                    'Forgot password?',
+                    style: MyStyles.textStyle14.copyWith(
+                      decoration: TextDecoration.underline,
+                      color: MyColors.mainColor
+                    ),
+                  ),
                 ],
-              )
+              ),
+              SizedBox(
+                height: 25.h,
+              ),
+
             ],
           ),
         ),
